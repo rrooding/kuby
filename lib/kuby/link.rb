6a02dcbbf8f26584d847c61e9cbb33b4bf0c8373
Ralph Rooding <ralph@izerion.com>
2013-08-29 17:27:59 +0200

    Added specs for initializer

--- lib/kuby/link.rb
+++ lib/kuby/link.rb
@@ -1,3 +1,8 @@
 class Kuby::Link
+  attr_reader :host, :port
 
+  def initialize(options={})
+    @host = options.fetch(:host, '127.0.0.1')
+    @port = options.fetch(:port, 8085).to_i
+  end
 end
--- spec/kuby/link_spec.rb
+++ spec/kuby/link_spec.rb
@@ -1,5 +1,33 @@
 require 'spec_helper'
 
 describe Kuby::Link do
+  subject { Kuby::Link.new }
 
+  describe '#initialize' do
+    context 'without options' do
+      it 'sets the host to the default' do
+        expect(subject.host).to eq '127.0.0.1'
+      end
+
+      it 'sets the port to default' do
+        expect(subject.port).to eq 8085
+      end
+    end
+
+    context 'with host' do
+      subject { Kuby::Link.new(host: 'kerbalhost.com') }
+
+      it 'sets the host to the given value' do
+        expect(subject.host).to eq 'kerbalhost.com'
+      end
+    end
+
+    context 'with port' do
+      subject { Kuby::Link.new(port: 1234) }
+
+      it 'sets the port to the given value' do
+        expect(subject.port).to eq 1234
+      end
+    end
+  end
 end
