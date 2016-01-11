18d8f1ec0d492c4364d82f3f2b431294ea504558
Russell Osborne <russell@theironyard.com>
2016-01-11 11:51:48 -0500

    Implementing Pitch, Yaw, Roll, FWB, and translation commands

--- spec/kuby/link/flight_methods_spec.rb
+++ spec/kuby/link/flight_methods_spec.rb
@@ -37,6 +37,70 @@ describe Kuby::Link::FlightMethods do
     end
   end
 
+  describe '#pitch' do
+    it 'implements the v.setPitch call' do
+      expect(subject).to receive(:api_set).with('v.setPitch', 0.25)
+
+      subject.pitch = 45.0
+    end
+
+    context 'throttle is larger than 1' do
+      it 'normalizes to 1.0' do
+        expect(subject).to receive(:api_set).with('v.setPitch', 1.0)
+
+        subject.pitch=370
+      end
+    end
+
+    context 'throttle is not a float or int' do
+      it 'raises an ArgumentError' do
+        expect {
+          subject.pitch='a'
+        }.to raise_error ArgumentError
+      end
+    end
+
+    context 'throttle is smaller than 1' do
+      it 'normalizes to 0.0' do
+        expect(subject).to receive(:api_set).with('v.setPitch', -1.0)
+
+        subject.pitch = -380
+      end
+    end
+  end
+
+  describe '#pitch_yaw_roll_xyz' do
+    it 'implements the v.PitchYawRollXYZ call' do
+      expect(subject).to receive(:api_set).with('v.PitchYawRollXYZ', [0.5, 0.5, 0.5, 0.5, 0.5, 0.5] )
+
+      subject.pitch_yaw_roll_xyz = { pitch:90.0, yaw:90.0, roll:90.0, x:0.5, y:0.5, z:0.5}
+    end
+
+    context 'pitch is larger than 180' do
+      it 'normalizes to 1.0' do
+        expect(subject).to receive(:api_set).with('v.PitchYawRollXYZ', [1.0, 1.0, 1.0, 1.0, 1.0, 1.0])
+
+        subject.pitch_yaw_roll_xyz = { pitch: 370, yaw: 370, roll: 370, x: 370, y: 370, z: 370 }
+      end
+    end
+
+    context 'pitch is not a float or int' do
+      it 'raises an ArgumentError' do
+        expect {
+          subject.pitch_yaw_roll_xyz = {pitch:'a', yaw:'a', roll:'a', x:'a', y:'a', z:'a'}
+        }.to raise_error ArgumentError
+      end
+    end
+
+    context 'pitch is smaller than 0' do
+      it 'normalizes to 0.0' do
+        expect(subject).to receive(:api_set).with('v.PitchYawRollXYZ', [-0.75,-0.75,-0.75,0,0,0])
+
+        subject.pitch_yaw_roll_xyz = {pitch:-135.0, yaw:-135.0, roll:-135.0, x:-0.4, y:-0.4, z:-0.4}
+      end
+    end
+  end
+
   describe '#set_throttle' do
     it 'implements the f.setThrottle call' do
       expect(subject).to receive(:api_set).with('f.setThrottle', 0.5)
